d99ca4aed49fb9647e778ab2db67b9c8d1a74aaa
Volage <79306744+Volagejs@users.noreply.github.com>
2022-04-17 13:30:12 +0300

    a

--- events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client, interaction) => {
-    console.log(`${client.user.tag} ismi ile bot giriş yaptı.`);
-    console.log(`Başarılı.`);
-    client.user.setPresence({activities: [{name:"Yenileniyor..."}], status:"dnd"});   
-};
